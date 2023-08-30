2d31788d32c9a6d64e1c41368c330a2d60ab8413
Tanjina Akter Nisa <140683300+tanjinanisa86@users.noreply.github.com>
2023-08-30 15:26:22 +0600

    Add files via upload

---
+++ function5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+LIKE(int x){
+    int i;
+    for(i=1;i<=x;i++)
+        {
+printf("Like ");
+        }
+}
+int main (){
+	int x;
+	scanf("%d",&x);
+    printf("I ");
+    LIKE(x);
+    printf("You");
+
+}
+
